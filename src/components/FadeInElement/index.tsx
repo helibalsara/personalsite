e3fe320d30c6807a23be7d0fbbea9156f912abb1
Shamith Pasula <shamith09@gmail.com>
2024-01-21 19:37:50 -0800

    fade

---
+++ src/components/FadeInElement/index.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+import "./index.css";
+
+const FadeInElement: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const elementRef = useRef<HTMLDivElement | null>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Function to check if element is in viewport
+  const isElementInViewport = (element: HTMLDivElement | null) => {
+    if (!element) return false;
+    const rect = element.getBoundingClientRect();
+    const height = rect.bottom - rect.top;
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom - height / 2 <= window.innerHeight &&
+      rect.right <= window.innerWidth
+    );
+  };
+
+  // Function to handle the scroll event
+  const handleScroll = () => {
+    if (isElementInViewport(elementRef.current)) {
+      setIsVisible(true);
+      window.removeEventListener("scroll", handleScroll);
+    }
+  };
+
+  // Add a scroll event listener when the component mounts
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={elementRef}
+      className={`fade-in-element ${isVisible ? "visible" : ""}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default FadeInElement;
--- src/components/ProjectTile/index.tsx
+++ src/components/ProjectTile/index.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./index.module.css";
 import Link from "next/link";
-
+import FadeInElement from "../FadeInElement";
 interface Props {
   roleType: string;
   title: string;
@@ -21,21 +21,23 @@ export default function ProjectTile({
 }: Props) {
   return (
     <Link href={"/projects" + route} className={styles.link}>
-      <div className={styles.container}>
-        <Image
-          src={image}
-          alt={title}
-          width={576}
-          height={337}
-          className={styles.image}
-        />
-        <div className={styles.textContainer}>
-          <p className={styles.roleType}>{roleType}</p>
-          <h1 className={styles.title}>{title}</h1>
-          <h3 className={styles.subtitle}>{subtitle}</h3>
-          <p className={styles.description}>{description}</p>
+      <FadeInElement>
+        <div className={styles.container}>
+          <Image
+            src={image}
+            alt={title}
+            width={576}
+            height={337}
+            className={styles.image}
+          />
+          <div className={styles.textContainer}>
+            <p className={styles.roleType}>{roleType}</p>
+            <h1 className={styles.title}>{title}</h1>
+            <h3 className={styles.subtitle}>{subtitle}</h3>
+            <p className={styles.description}>{description}</p>
+          </div>
         </div>
-      </div>
+      </FadeInElement>
     </Link>
   );
 }

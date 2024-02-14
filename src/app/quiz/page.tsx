58613001848954cdb8f3eaa1fbf8afa8130bb52d
Shamith Pasula <shamith09@gmail.com>
2024-02-14 09:57:42 -0800

    final

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -2,7 +2,7 @@
 
 import Button from "@/components/Button";
 import styles from "./page.module.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import RadioButton from "@/components/RadioButton";
 import Hearts from "./hearts.svg";
 
@@ -47,12 +47,58 @@ export default function Quiz() {
   const [selectedOption, setSelectedOption] = useState<number>();
   const [wrong, setWrong] = useState<boolean>(false);
   const [submitFlipper, setSubmitFlipper] = useState<boolean>(false);
+  const [showGo, setShowGo] = useState<boolean>(false);
+  const [endGo, setEndGo] = useState<boolean>(false);
 
   const calculateBackground = () => {
     const percentageFilled = (index / questions.length) * 100; // Calculate the percentage of the container to be filled
 
     return { height: `${percentageFilled}%` };
   };
+
+  const letsRef = useRef<HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    const letsEl = letsRef.current;
+    if (!letsEl) return;
+
+    const handleAnimationEnd = () => {
+      setShowGo(true); // Show the "go" text
+      letsEl.style.display = "none"; // Hide the "Let's" text
+    };
+
+    if (letsEl) {
+      letsEl.addEventListener("animationend", handleAnimationEnd);
+    }
+
+    return () => {
+      if (letsEl) {
+        letsEl.removeEventListener("animationend", handleAnimationEnd);
+      }
+    };
+  }, [index]);
+
+  const goRef = useRef<HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    const goEl = goRef.current;
+    if (!goEl) return;
+
+    const handleAnimationEnd = () => {
+      setEndGo(true);
+      goEl.style.display = "none";
+    };
+
+    if (goEl) {
+      goEl.addEventListener("animationend", handleAnimationEnd);
+    }
+
+    return () => {
+      if (goEl) {
+        goEl.removeEventListener("animationend", handleAnimationEnd);
+      }
+    };
+  }, [showGo]);
 
   return (
     <div className={styles.container}>
@@ -67,8 +113,8 @@ export default function Quiz() {
                 We&apos;ve been together more than a year now! Before I ask you
                 to be my Valentine, I need to find out if you remember how we
                 got to this point. Don&apos;t worry, if you ever get stuck,
-                I&apos;ll remind you of the answers because I love thinking
-                about us. Good luck! :)
+                I&apos;ll remind you of the answers because I love talking about
+                us. Good luck! :)
               </p>
             )}
 
@@ -117,6 +163,41 @@ export default function Quiz() {
             >
               <p>{index == -1 ? "Continue" : "Submit"}</p>
             </Button>
+          </div>
+        </div>
+      )}
+      {index >= questions.length && (
+        <div className={styles.end}>
+          <p ref={letsRef} className={styles.lets}>
+            Let&apos;s
+          </p>
+          {showGo && (
+            <p ref={goRef} className={styles.go}>
+              gooooooooooooooooooooo
+            </p>
+          )}
+        </div>
+      )}
+      {endGo && (
+        <div className={styles.row}>
+          <div className={styles.scrollContainer}>
+            <div className={styles.scroll1} />
+          </div>
+          <div className={styles.text}>
+            <h1>I love you so much {"<3"}</h1>
+            <p>
+              Heli, I&apos;m so happy we got to make so many memories together
+              already. I can&apos;t imagine a life without you anymore! I hope
+              we make so many more memories in the future, and I&apos;ll never
+              stop trying to make each and every moment together memorable.
+              You&apos;re the most beautiful, kindest, most thoughtful person I
+              have ever met. I&apos;m so lucky to have you in my life and I hope
+              you see that too.
+            </p>
+            <p className={styles.right}>Love, Shamith</p>
+          </div>
+          <div className={styles.scrollContainer}>
+            <div className={styles.scroll2} />
           </div>
         </div>
       )}

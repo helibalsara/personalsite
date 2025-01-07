9299284ee841265874e469d283f549d4a6f442fa
Shamith Pasula <shamith09@berkeley.edu>
2025-01-06 17:23:36 -0800

    response

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,15 +5,15 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <div className="flex flex-col gap-6 bg-[#e1e1e1] px-16 py-16">
-      <div className="flex flex-row justify-between gap-4 md:gap-[83px]">
-        <div className="flex flex-col gap-11 md:gap-6">
-          <h2 className="text-black text-4xl font-normal inline-block mr-16">
+      <div className="flex flex-col md:flex-row justify-between gap-4 md:gap-[83px]">
+        <div className="flex flex-col items-center md:items-start gap-6">
+          <h2 className="text-center md:text-left text-black text-3xl md:text-4xl font-normal">
             thanks for stopping by!
           </h2>
-          <p className="text-black text-xl font-normal">
+          <p className="text-center md:text-left text-black text-xl font-normal">
             Get in touch :)
           </p>
-          <div className="flex flex-row gap-4 items-center">
+          <div className="flex flex-row gap-4 items-center justify-center md:justify-start">
             <a
               href="https://www.linkedin.com/in/heli-balsara/" 
               target="_blank"
@@ -40,7 +40,7 @@ export default function Footer() {
             </a>
           </div>
         </div>
-        <div className="flex flex-grow items-center justify-end">
+        <div className="flex flex-grow items-center justify-center md:justify-end mt-4 md:mt-0">
           <button
             onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
             className="text-black text-xl font-normal inline-block cursor-pointer hover:opacity-60"

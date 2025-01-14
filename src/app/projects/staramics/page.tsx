42e3f2566ab3933ca4fc47b0e85dcb6e7784fa6b
Shamith Pasula <shamith09@berkeley.edu>
2025-01-14 01:41:48 -0800

    staramics

--- src/app/projects/staramics/page.tsx
+++ src/app/projects/staramics/page.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import WIP from "@/components/WIP";
+import ProjectOverview from "@/components/ProjectOverview";
+import Line from "@/components/Line";
 
 export default function Staramics() {
   return (
@@ -17,8 +20,100 @@ export default function Staramics() {
           priority
         />
       </div>
-      <WIP />
-      <Footer />
+      <ProjectOverview
+        role="Designer"
+        duration="January 2024 - May 2024"
+        disciplines="Brand Identity, Graphic Design"
+        description={
+          <>
+            <strong>
+              Staramics is a ceramics studio that creates long-lasting
+              decorative mini ceramic animals that are both whimsical and
+              collectable. It helps people showcase their individual
+              personalities.
+            </strong>
+            <br />
+            <br />
+            This is a brand created in the decal Brand Identity and Graphic
+            Design. I wanted to take a fun and whimsical approach to this brand
+            since I have only worked with web/ mobile UI. It was a great way for
+            me to exercise my creativity and work with Illustrator.
+          </>
+        }
+      />
+      <div className="flex flex-col gap-8 px-[182px]">
+        <h1 className="text-[32px] font-bold">Logo & Color Palette</h1>
+        <div className="flex flex-row gap-8">
+          <Image
+            src="/staramics/logocolor.svg"
+            width={576}
+            height={336.75}
+            style={{ height: "auto", width: "100%" }}
+            alt="staramics"
+          />
+          <p className="text-base">
+            I chose warm and earthy colors to bring a warm, fuzzy feeling to the
+            brand. I went with a fun font to showcase the playful nature of the
+            studio’s products. Editing the font slightly to extend the “r” to
+            hug the “a” further highlights the fun! I chose a cat hugging a star
+            for the main mascot of this brand since it brought the playfulness
+            to another level.
+          </p>
+        </div>
+        <Line className="my-8" />
+        <h1 className="text-[32px] font-bold">Additional Assets</h1>
+        <div className="flex flex-row gap-2">
+          <div>
+            <Image
+              src="/staramics/assets1.png"
+              width={504 * 4}
+              height={289 * 4}
+              style={{ height: "auto", width: "100%" }}
+              alt="staramics"
+            />
+          </div>
+          <div>
+            <Image
+              src="/staramics/assets2.png"
+              width={404 * 4}
+              height={289 * 4}
+              style={{ height: "auto", width: "100%" }}
+              alt="staramics"
+            />
+          </div>
+        </div>
+        <div className="flex flex-row gap-2 -mt-6">
+          <div>
+            <Image
+              src="/staramics/assets3.png"
+              width={403 * 4}
+              height={289 * 4}
+              style={{ height: "auto", width: "100%" }}
+              alt="staramics"
+            />
+          </div>
+          <div>
+            <Image
+              src="/staramics/assets4.png"
+              width={505 * 4}
+              height={289 * 4}
+              style={{ height: "auto", width: "100%" }}
+              alt="staramics"
+            />
+          </div>
+        </div>
+        <Line className="my-8" />
+        <h1 className="text-[32px] font-bold">Reflection</h1>
+        <p className="text-base">
+          Staramics helped spark a sense of playful creativity for me. It was
+          fulfilling to create a whole brand from scratch, without any pressure
+          of deliverables. It reconnected me to the fun of designing and allowed
+          me to explore another medium of art I enjoy - ceramics. I wanted this
+          brand to be collectables like Smiskis and Sonny Angels, but in ceramic
+          form.
+        </p>
+      </div>
+      <Footer className="mt-[177px]" />
     </>
   );
 }
--- src/components/ProjectOverview/index.tsx
+++ src/components/ProjectOverview/index.tsx
@@ -1,9 +1,9 @@
 interface ProjectOverviewProps {
-  role: string;
-  duration: string;
-  disciplines: string;
-  team: string;
-  description: React.ReactNode;
+  role?: string;
+  duration?: string;
+  disciplines?: string;
+  team?: string;
+  description?: React.ReactNode;
 }
 
 export default function ProjectOverview({
@@ -11,27 +11,21 @@ export default function ProjectOverview({
   duration,
   disciplines,
   team,
-  description
+  description,
 }: ProjectOverviewProps) {
   return (
     <div className="flex flex-row w-full gap-[94px] px-[182px] py-16">
       <div className="flex flex-col gap-4 flex-1">
-        <div className="flex flex-col gap-1">
-          <h3 className="font-bold">Role</h3>
-          <p className="text-base">{role}</p>
-        </div>
-        <div className="flex flex-col gap-1">
-          <h3 className="font-bold">Duration</h3>
-          <p className="text-base">{duration}</p>
-        </div>
-        <div className="flex flex-col gap-1">
-          <h3 className="font-bold">Disciplines</h3>
-          <p className="text-base">{disciplines}</p>
-        </div>
-        <div className="flex flex-col gap-1">
-          <h3 className="font-bold">Team</h3>
-          <p className="text-base">{team}</p>
-        </div>
+        {[role, duration, disciplines, team]
+          .filter((item) => item !== undefined)
+          .map((item, index) => (
+            <div className="flex flex-col gap-1" key={index}>
+              <h3 className="font-bold">
+                {["Role", "Duration", "Disciplines", "Team"][index]}
+              </h3>
+              <p className="text-base">{item}</p>
+            </div>
+          ))}
       </div>
       <div className="flex-1">
         <h2 className="text-xl">{description}</h2>
